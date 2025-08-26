7f98c601fd1f78196b478a9043ff288379132f8d
haku <hakubhai6871@gmail.com>
2025-08-26 12:08:08 +0530

    script.js

--- script.js
+++ script.js
@@ -7,18 +7,17 @@ const currentTheme = localStorage.getItem('theme') || 'dark';
 
 // Apply saved theme
 if (currentTheme === 'light') {
-body.classList.add('light-theme');
-themeToggle.checked = true;
+  body.classList.add('light-theme');
+  themeToggle.checked = true;
 }
 
 // Toggle theme on checkbox change
 themeToggle.addEventListener('change', function() {
-if (this.checked) {
-body.classList.add('light-theme');
-localStorage.setItem('theme', 'light');
-} else {
-body.classList.remove('light-theme');
-localStorage.setItem('theme', 'dark');
-}
+  if (this.checked) {
+    body.classList.add('light-theme');
+    localStorage.setItem('theme', 'light');
+  } else {
+    body.classList.remove('light-theme');
+    localStorage.setItem('theme', 'dark');
+  }
 });
-
